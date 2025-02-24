6e17d76fb0a102cade01e5f8117aa1eb69626c99
reham nasr <rnasr3667@gmail.com>
2025-02-24 09:14:54 +0200

    added countdown function

--- test1.py
+++ test1.py
@@ -10,12 +10,23 @@ def start_game():
     colour_text["text"]= colours[0]
     colour_text["fg"]=colours[1]
     random.shuffle(colours)
+    timedown()
+
+def timedown():
+    global time
+    if time > 0:
+        time = time-1
+        time_lbl["text"] = "time left = " + str(time)
+        time_lbl.after(1000, timedown)
+
+
+
 txt1=Label(text="type in the colour of the wotd not the word text", font=("bold", 20))
 txt1.pack()
 txt2=Label(text="press enter to start", font=("bold", 20))
 txt2.pack()
-time=Label(text="time left = 30", font=("bold", 20))
-time.pack()
+time_lbl=Label(text="time left = 30", font=("bold", 20))
+time_lbl.pack()
 colour_text = Label(text="", font=("bold", 40))
 colour_text.pack()
 ent = Entry(width=40)
